109b5ebde6cec3aa0488e3a684bd6cb1293f4dec
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-05-13 12:12:00 +0530

    add default front end and connected to mongoDB

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors'
+
+dotenv.config();
+
+const app=express();
+const PORT = process.env.PORT || '4000';
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL=process.env.MONOGODB_URL;
+
+mongoose.connect(URL,
+   
+);
+
+const connection = mongoose.connection;
+connection.once("open",()=>{
+    console.log('MongoDB is connected');
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port:${PORT}`)
+})
+
