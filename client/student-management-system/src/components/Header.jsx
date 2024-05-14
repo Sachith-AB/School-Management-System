eb43b38fc506c62f44b7ce39c99a235e1e59d934
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-05-14 14:13:58 +0530

    complete student adding page

--- client/student-management-system/src/components/Header.jsx
+++ client/student-management-system/src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
             School Management System
           </span>
         </Link>
-      <div className='max-w-screen-xl flex flex-wrap justify-between mx-auto p-4'>
+      <div className='max-w-screen-xl flex flex-wrap  mx-auto p-4'>
         
         <Button
           onClick={() => toggleDropdown()}
@@ -44,7 +44,7 @@ export default function Header() {
             <li className='relative'>
               <button
                 onClick={toggleMenu}
-                className='flex justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent'
+                className=' w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent'
               >
                  Menu
                 <svg className='w-2.5 h-2.5 ml-2.5' aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
--- client/student-management-system/src/pages/CreatePage.jsx
+++ client/student-management-system/src/pages/CreatePage.jsx
@@ -1,7 +1,117 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 export default function CreatePage() {
+  const[formData,setFormData]=useState({});
+
+  const handleSumbit= async(e)=>{
+        e.preventDefault();
+        
+      try{
+        const res=await axios.post('http://localhost:4000/api/student/add',formData);
+        alert("student added");
+        console.log(res.data);
+      }
+      catch(error){
+        alert(error);
+      }
+      
+        }
+       
+
   return (
-    <div>CreatePage</div>
+    <div className="w-full max-w-xs m-28 ">
+  <form className="bg-white shadow-md rounded px-20 py-10 w-full">
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="studentname" >
+        Student Name
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="name"
+      onChange={(e)=>{
+        setFormData({
+          ...formData,name:e.target.value
+        })
+      }}/>
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Age
+      </label>
+      <input className="shadow appearance-none border border-gray-700 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="" type="text" placeholder="age"
+       onChange={(e)=>{
+        setFormData({
+          ...formData,age:e.target.value
+        })
+      }}/>
+      
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Gender
+      </label>
+      <input className="shadow appearance-none border border-gray-700 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="text" type="text" placeholder="gender"
+       onChange={(e)=>{
+        setFormData({
+          ...formData,gender:e.target.value
+        })
+      }}/>
+      
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Address
+      </label>
+      <input className="shadow appearance-none border border-gray-700 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"  type="text" placeholder="address"
+       onChange={(e)=>{
+        setFormData({
+          ...formData,address:e.target.value
+        })
+      }}/>
+      
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Phone No
+      </label>
+      <input className="shadow appearance-none border border-grat-700 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" placeholder="Tel"
+       onChange={(e)=>{
+        setFormData({
+          ...formData,phoneNumber:e.target.value
+        })
+      }}/>
+      
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Stream
+      </label>
+      <input className="shadow appearance-none border border-gray-700 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" placeholder="stream"
+       onChange={(e)=>{
+        setFormData({
+          ...formData,scheme:e.target.value
+        })
+      }}/>
+      
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Email
+      </label>
+      <input className="shadow appearance-none border border-gray-700 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="email" placeholder="email"
+       onChange={(e)=>{
+        setFormData({
+          ...formData,email:e.target.value
+        })
+      }}/>
+      
+    </div>
+    
+   <button className='bg-blue-700 px-2 py-1 rounded-lg text-white font-semibold' onClick={handleSumbit}>
+    Submit
+   </button>
+  </form>
+  
+</div>
   )
 }
