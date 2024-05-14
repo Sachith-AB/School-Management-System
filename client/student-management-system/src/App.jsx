cb2997ce02f823aea2dcb4e4535dcd17ceccf7f6
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-05-14 08:22:45 +0530

    complete pages

--- client/student-management-system/src/App.jsx
+++ client/student-management-system/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
+import DeletePage from './pages/DeletePage';
+import UpdatePage from './pages/UpdatePage';
+import CreatePage from './pages/CreatePage';
+import SearchPage from './pages/SearchPage';
 
 export default function App() {
   return (
@@ -9,6 +13,10 @@ export default function App() {
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/deletestudent" element={<DeletePage/>}/>
+      <Route path="/updatestudent" element={<UpdatePage/>}/>
+      <Route path="/createstudent" element={<CreatePage/>}/>
+      <Route path="/search" element={<SearchPage/>}/>
     </Routes>
     </BrowserRouter>
   )
